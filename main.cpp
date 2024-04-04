93cf11717ae0a09299e45956134d49c0d086d71a
Unknown <lucas.marcel.123@LA30712060280L.imd.local>
2024-04-04 18:16:31 -0300

    salva

--- main.cpp
+++ main.cpp
@@ -3,7 +3,44 @@
 
 using namespace std;
 
+template <typename T> class List {
+    private:
+        unsigned int qtd = 0;
+        T** items;
+    public:
+        List<T>() {
+            this->items = new T[0];
+        };
+        add(T* item) {
+            this->qtd++;
+            T** items = new T[qtd];
+            for(int i = 0; i < qtd - 1; i++) {
+                items[i] = 
+            };
+        };
+};
 
+class Astronaut {
+    private:
+        Expedition** expeditions;
+        bool available = true;
+        string cpf;
+        string name;
+
+};
+
+class Expedition {
+    private:
+
+        Astronaut** astronauts;
+        string name;
+    public:
+        Expedition() {
+            this->astronauts = new Astronaut[0];
+        };
+
+
+};
 
 int main() {
     srand(time(NULL));
